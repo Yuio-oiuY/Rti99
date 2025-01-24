b4b0826a9ecc99c5cc87b462b1db6747b268e2b9
Andrey <poizonFF@mail.ru>
2025-01-24 20:46:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using SquareLibrary;
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Демонстрация работы с библиотекой SquareLibrary");
+        Console.WriteLine("---------------------------------------------\n");
+
+        Console.WriteLine("Введите сторону квадрата:");
+        if (double.TryParse(Console.ReadLine(), out double a))
+        {
+            try
+            {
+                Square square = new Square(a);
+                Square square1 = new Square(a);
+                Console.WriteLine($"Сторона квадрата : {square.Side}");
+                Console.WriteLine($"Площадь квадрата равна: {square.Area()}");
+                Console.WriteLine($"Периметр квадрата равен : {square1.Perimeter()}");
+
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
+        }
+
+
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareLibrary
+{
+    public interface IShape
+    {
+        double Area();
+        double Perimeter();
+    }
+
+    public class Square : IShape
+    {
+        
+        
+            public double Side { get; set; }
+
+            public Square(double side)
+            {
+                if (side <= 0)
+                {
+                    throw new ArgumentException("Сторона квадрата должна быть положительным числом.");
+                }
+                Side = side;
+            }
+
+            public double Area()
+            {
+                return Side * Side;
+            }
+
+            public double Perimeter()
+            {
+                return 4 * Side;
+            }
+        
+
+        double IShape.Area() => this.Area();
+        double IShape.Perimeter() => this.Perimeter();
+    }
+}
